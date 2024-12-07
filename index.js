ad96b70a07ed8173a1c6c9004902bf384a415036
AndrewBlack21 <andrew.moraes16@hotmail.com>
2024-12-06 22:13:58 -0300

    "Arrumando o bug do dropdown"

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+// Alterna o menu mobile
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+};
+
+// Fecha o menu ao rolar a página
+window.onscroll = () => {
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+};
+
+// Adiciona suporte ao dropdown no menu mobile
+const dropdown = document.querySelector('.dropdown');
+const dropdownMenu = dropdown.querySelector('.dropdown-menu');
+
+dropdown.addEventListener('mouseover',   
+() => {
+  dropdownMenu.style.display = 'block';
+});
+
+dropdown.addEventListener('mouseout', () => {
+  dropdownMenu.style.display   
+= 'none';
+});
+
+
+
+
 class FormSubmit {
   constructor(settings) {
     this.settings = settings;
@@ -64,34 +97,4 @@ const formHandler = new FormSubmit({
 });
 
 formHandler.init();
- 
-
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .navbar');
-
-// Alterna o menu mobile
-menu.onclick = () => {
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
-};
-
-// Fecha o menu ao rolar a página
-window.onscroll = () => {
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
-};
-
-// Adiciona suporte ao dropdown no menu mobile
-const dropdown = document.querySelector('.dropdown');
-const dropdownMenu = dropdown.querySelector('.dropdown-menu');
-
-dropdown.addEventListener('mouseover',   
-() => {
-  dropdownMenu.style.display = 'block';
-});
-
-dropdown.addEventListener('mouseout', () => {
-  dropdownMenu.style.display   
-= 'none';
-});
 
